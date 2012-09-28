efb4964802ea3c76a2cb3474b9fb2d2b657813c0
Russ Profant <russ@datastreamprocessor.com>
2012-09-28 19:19:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '68d69a362ffe65a6f84b5fffdee1df4cf25c313fec3e88abca1c48c62cb77fd239d59f9ab0a8ed07ba1f0fedcf12a7fd9d07fbc6a150d93c1af7c847dbbfdfba'
